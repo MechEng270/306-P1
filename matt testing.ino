6803ef94c650a7b4be4055060ed1f2002bd66bab
MechEng270 <109499072+MechEng270@users.noreply.github.com>
2023-08-08 17:57:05 +1200

    Add files via upload
    gg 8/8/23

--- matt testing.ino
+++ matt testing.ino
@@ -8,6 +8,8 @@ int right = 19;
 int bottom = 3;
 int top =2;
 
+int x = 0;
+int y = 0;
 bool L, R, B, T = 0;
 
 
@@ -18,7 +20,8 @@ volatile bool dir;
 
 volatile bool SetLeft = true;
 volatile bool SetBottom = true;
-
+volatile bool SetRight = true;
+volatile bool SetUp = true;
 void setup()
 {
 Serial.begin(9600);
@@ -35,21 +38,156 @@ attachInterrupt(digitalPinToInterrupt(right), safeR, HIGH);
 attachInterrupt(digitalPinToInterrupt(bottom), safeB, HIGH);
 attachInterrupt(digitalPinToInterrupt(top), safeT, HIGH);
 
-
+homie();
+countL = 0;
+countR = 0;
+//goRight();
+//goUp();
+//PIDtest();
+//SetRight = 1;
 }
 void loop()
 {
- 
-    // digitalWrite(M1, LOW);
-    // digitalWrite(M2, LOW);
-    // analogWrite(E1, 255); //PWM Speed Control
-    // analogWrite(E2, 255);
+  digitalWrite(M1, LOW);
+  digitalWrite(M2, LOW);
+  analogWrite(E1, 0); //PWM Speed Control
+  analogWrite(E2, 0);
+}
 
-// digitalWrite(M1,HIGH);
-// digitalWrite(M2,LOW);
-// analogWrite(E1, 0); //PWM Speed Control
-// analogWrite(E2, 0); //PWM Speed Control
+// void Rectangle(Width,Height){
 
+//   W =  Width* 6050
+//   H = Height* 9557
+
+// }
+
+// void circle(float radius, float xIn, float yIn){
+
+
+
+
+
+// PIDtest
+
+// }
+
+void goRight(){
+while(SetRight){
+     x = (countL + countR)/2;
+     y = (countL - countR)/2;
+    digitalWrite(M1, LOW);
+    digitalWrite(M2, LOW);
+    analogWrite(E1, 255); //PWM Speed Control
+    analogWrite(E2, 255);
+    Serial.print(x);
+      Serial.print(", x");
+      Serial.println();
+  }
+     x = (countL + countR)/2;
+     y = (countL - countR)/2;
+      Serial.print(x);
+      Serial.print(", x");
+      Serial.println();
+}
+
+void goUp(){
+while(SetUp){
+     x = (countL + countR)/2;
+     y = (countL - countR)/2;
+    digitalWrite(M1,LOW);
+    digitalWrite(M2, HIGH);
+    analogWrite(E1, 255); //PWM Speed Control
+    analogWrite(E2, 255);
+      Serial.print(y);
+      Serial.print(", y");
+      Serial.println();
+  }
+     x = (countL + countR)/2;
+     y = (countL - countR)/2;
+      Serial.print(y);
+      Serial.print(", y");
+      Serial.println();
+
+}
+
+void PIDtest(float xIn, float yIn){
+  float error, integral, lastE, der = 0, effort;
+  //float xIn = 6000.0;
+  //float yIn = 2000.0;
+  float kp = 2, ki = 1, kd = 0.3;
+
+  while(error > (xIn * .02)){
+    Serial.print(error);
+    Serial.print(",  ");
+    Serial.print(effort);
+    Serial.print(",  ");
+    Serial.println(x);
+      x = (countL + countR)/2;
+      error = xIn - x;
+      integral = integral + error;
+      der = error - lastE;
+      lastE = error;
+      if(error > 0){ //move right
+      digitalWrite(M1, LOW);
+      digitalWrite(M2, LOW);
+      }else if(error < 0){ //move left
+      digitalWrite(M1,HIGH);
+      digitalWrite(M2,HIGH);
+
+  }
+  effort = 255 * ((kp * error) + (ki * integral));
+  if(effort > 255){
+    effort = 255;
+  }
+      analogWrite(E1, effort);
+      analogWrite(E2, effort);
+    
+    delay(100);
+  }
+
+  // while(error > (yIn * .02)){
+  //   Serial.print(error);
+  //   Serial.print(",  ");
+  //   Serial.print(effort);
+  //   Serial.print(",  ");
+  //   Serial.println(y);
+  //     y = (countL - countR)/2;
+  //     error = yIn - y;
+  //     integral = integral + error;
+  //     der = error - lastE;
+  //     lastE = error;
+  //     if(error > 0){ //move right
+  //     digitalWrite(M1,LOW);
+  //     digitalWrite(M2, HIGH);
+  //     }else if(error < 0){ //move left
+  //     digitalWrite(M1,HIGH);
+  //     digitalWrite(M2, LOW);
+
+  // }
+  // effort = 255 * ((kp * error) + (ki * integral));
+  // if(effort > 255){
+  //   effort = 255;
+  // }
+  //     analogWrite(E1, effort);
+  //     analogWrite(E2, effort);
+    
+  //   delay(100);
+  // }
+      Serial.print(error);
+      Serial.print(",  ");
+      Serial.print(effort);
+      Serial.print(",  ");
+      Serial.println(x);
+      analogWrite(E1, 0);
+      analogWrite(E2, 0);
+      Serial.print(error);
+      Serial.print(",  ");
+      Serial.println(x);
+      
+
+}
+
+void homie(){
 while(SetLeft){
   digitalWrite(M1,HIGH);
   digitalWrite(M2,HIGH);
@@ -65,67 +203,7 @@ while(SetBottom){
   analogWrite(E2, 100); //PWM Speed Control
   Serial.println("going butt");
 }
-
-
-
-
-// while(count < 2064){
-//   digitalWrite(M1,HIGH);
-//   //digitalWrite(M2, HIGH);
-//   if(count < 1300){
-//     analogWrite(E1, 100); //PWM Speed Control
-//     analogWrite(E2, 100); //PWM Speed Control
-//   }
-//   else{
-//     analogWrite(E1, 50); //PWM Speed Control
-//     analogWrite(E2, 50); //PWM Speed Control
-//   }
-//   Serial.println(count);
-  
-// }
-// analogWrite(E1, 0); //PWM Speed Control
-// analogWrite(E2, 0); //PWM Speed Control
-// delay(3000);
-// count = 0;
-// while(count > -2064){
-//   digitalWrite(M1,LOW);
-//   digitalWrite(M2, LOW);
-//   if(count > -1300){
-//     analogWrite(E1, 100); //PWM Speed Control
-//     analogWrite(E2, 100); //PWM Speed Control
-//   }
-//   else{
-//     analogWrite(E1, 50); //PWM Speed Control
-//     analogWrite(E2, 50); //PWM Speed Control
-//   }
-//   Serial.println(countL);
-//   Serial.println(countR);
-// }
-
-
-
-
-
-// Serial.println("rev completed");
-//   analogWrite(E1, 0); //PWM Speed Control
-//   analogWrite(E2, 0);
-// delay(3000);
-// Serial.println(count);
-// count = 0;
-
 }
-
-// void Rectangle(Width,Height){
-
-//   W =  Width*
-//   H = Height*
-
-// }
-
-
-
-
-
 
 
 void safeR(){
@@ -137,6 +215,7 @@ void safeR(){
   }
   analogWrite(E1, 0); //PWM Speed Control
   analogWrite(E2, 0); 
+  SetRight = false;
   //Serial.println(count); 
 }
 
@@ -175,6 +254,7 @@ void safeT(){
   }
   analogWrite(E1, 0); //PWM Speed Control
   analogWrite(E2, 0); 
+  SetUp = false;
 }
 
 
